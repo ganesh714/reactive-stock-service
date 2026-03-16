8506011f1e0a8f45008f9d79cf9fdc6803fbe11d
ganesh714 <evvganesh1@gmail.com>
2026-03-16 08:08:50 +0530

    Initial commit: Spring WebFlux stock service setup

---
+++ src/main/java/com/software/reactive_stock_service/ReactiveStockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.software.reactive_stock_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStockServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/software/reactive_stock_service/ReactiveStockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.reactive_stock_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
